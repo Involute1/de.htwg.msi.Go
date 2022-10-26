3c0bd3e0456be26bafab0d802318041e4a133451
Luis.Nothvogel <L_noth@yahoo.de>
2022-10-26 18:55:41 +0200

    working on first controller impl

--- src/main/scala/de/htwg/msi/controller/GameController.scala
+++ src/main/scala/de/htwg/msi/controller/GameController.scala
@@ -1,12 +1,53 @@
 package de.htwg.msi.controller
 
 class GameController extends TGameController {
+  val controllerState: TControllerState = PreInitGameState(this)
   override def eval(input: String): Unit = {
-    println(input)
+    controllerState.evaluate(input)
     notifyObservers()
   }
 
   override def printGameBoard(): String = ???
 
   override def printActions(): String = ???
+}
+
+trait TControllerState {
+  def evaluate(input: String): Unit
+  def nextState: TControllerState
+
+}
+
+case class PreInitGameState(controller: GameController) extends TControllerState {
+  override def evaluate(input: String): Unit = {
+    //TODO init gameboard 11x11 9x9 19x19
+
+  }
+
+  override def nextState: TControllerState = PlayerSetupState(controller)
+}
+
+case class PlayerSetupState(controller: GameController) extends TControllerState {
+  override def evaluate(input: String): Unit = {
+    //TODO init players
+  }
+
+  override def nextState: TControllerState = PlayingState(controller)
+}
+
+case class PlayingState(controller: TGameController) extends TControllerState {
+  override def evaluate(input: String): Unit = {
+    //TODO place stone
+    // forfeit
+  }
+
+  override def nextState: TControllerState = GameOverState(controller)
+}
+
+case class GameOverState(controller: TGameController) extends TControllerState {
+  override def evaluate(input: String): Unit = {
+    //TODO count score
+  }
+
+  override def nextState: TControllerState = this
 }
