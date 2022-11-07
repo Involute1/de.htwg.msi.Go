b80f3c61553f273922c0497e67607f23642632a3
da721wol <davwol@gmx.de>
2022-11-07 08:15:07 +0100

    Remove useless equals

--- src/main/scala/de/htwg/msi/model/Chain.scala
+++ src/main/scala/de/htwg/msi/model/Chain.scala
@@ -2,11 +2,4 @@ package de.htwg.msi.model
 
 case class Chain(set: Set[Field], liberties: Int) {
 
-  override def equals(obj: Any): Boolean = {
-    obj match {
-      case chain: Chain => chain.liberties == liberties && chain.set == set
-      case _ => false
-    }
-  }
-
 }
