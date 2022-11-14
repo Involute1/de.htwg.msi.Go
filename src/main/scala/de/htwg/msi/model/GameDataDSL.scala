f75ac36d808fb4916f141a4037ff56ac6f249ae5
Tobias Vogler <67332878+ToVo602@users.noreply.github.com>
2022-11-14 10:08:03 +0100

    internal dsl started

---
+++ src/main/scala/de/htwg/msi/model/GameDataDSL.scala
@@ -0,0 +1,29 @@
+package de.htwg.msi.model
+
+import de.htwg.msi.model.PlayerColor.{BLACK, WHITE}
+import de.htwg.msi.model.GameData.*
+
+class GameDataDSL(gameData: GameData) {
+
+  def sz(input: Int): GameData = {
+    gameData.copy(board = gameData.initBoard(input.toString))
+  }
+
+  def pw(name: String): GameData = {
+    //copy(players = Player(name, PlayerColor.WHITE) :: players)
+    ???
+  }
+
+  def pb(name: String): GameData = {
+    //copy(players = Player(name, PlayerColor.BLACK) :: players)
+    ???
+  }
+
+  def w(input: String): GameData = ???
+
+  def b(input: String): GameData = {
+    // Forfeit in sgf ist einfach ein leerer Zug
+    ???
+  }
+
+}
