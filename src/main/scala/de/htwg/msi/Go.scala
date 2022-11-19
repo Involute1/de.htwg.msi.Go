686cd58219cf1a6a1662a7e011fb918ea85f86da
Luis.Nothvogel <L_noth@yahoo.de>
2022-11-19 12:51:43 +0100

    internal dsl

--- src/main/scala/de/htwg/msi/Go.scala
+++ src/main/scala/de/htwg/msi/Go.scala
@@ -1,15 +1,16 @@
 package de.htwg.msi
 
 import de.htwg.msi.controller.{GameController, InitState}
+import de.htwg.msi.model.{GameData, GameDataDSL}
 import de.htwg.msi.view.Tui
 
 @main
 def main(args: String*): Unit = {
-  val tui: Tui = Tui();
-  tui.receiveUpdate(None)
-  var input: String = "";
-  while input != "q" do {
-    input = scala.io.StdIn.readLine()
-    tui.processInputLine(input)
-  }
+    val tui: Tui = Tui()
+    tui.receiveUpdate(None)
+    var input: String = ""
+    while input != "q" do {
+      input = scala.io.StdIn.readLine()
+      tui.processInputLine(input)
+    }
 }
--- src/main/scala/de/htwg/msi/model/GameDataDSL.scala
+++ src/main/scala/de/htwg/msi/model/GameDataDSL.scala
@@ -4,27 +4,50 @@ import de.htwg.msi.model.PlayerColor.{BLACK, WHITE}
 import de.htwg.msi.model.GameData.*
 
 class GameDataDSL(gameData: GameData) {
-  //TODO: implicit function like implicit def string2GameData(input) = new GameDataDSL() in main-method
-
   def sz(input: Int): GameData = {
     gameData.copy(board = gameData.initBoard(input.toString))
   }
 
+  def boardSizeIs(input: Int): GameData = {
+    sz(input)
+  }
+
   def pw(name: String): GameData = {
-    //copy(players = Player(name, PlayerColor.WHITE) :: players)
-    ???
+    gameData.copy(players = Player(name, PlayerColor.WHITE) :: gameData.players)
+  }
+
+  def playerWhiteIs(name: String): GameData = {
+    pw(name)
   }
 
   def pb(name: String): GameData = {
-    //copy(players = Player(name, PlayerColor.BLACK) :: players)
-    ???
+    gameData.copy(players = Player(name, PlayerColor.BLACK) :: gameData.players)
   }
 
-  def w(input: String): GameData = ???
+  def playerBlackIs(name: String): GameData = {
+    pb(name)
+  }
+
+  def whitePlaceStoneAt(input: String): GameData = {
+    w(input)
+  }
+
+  def w(input: String): GameData = {
+    // Forfeit in sgf ist einfach ein leerer Zug
+    if (input.isEmpty) return gameData.copy(turn = gameData.turn + 1)
+    if (!gameData.isMoveInputValid(input)) return gameData
+    gameData.copy(board = gameData.placeStone(input, WHITE), turn = gameData.turn + 1)
+  }
+
+  def blackPlaceStoneAt(input: String): GameData = {
+    b(input)
+  }
 
   def b(input: String): GameData = {
     // Forfeit in sgf ist einfach ein leerer Zug
-    ???
+    if (input.isEmpty) return gameData.copy(turn = gameData.turn + 1)
+    if (!gameData.isMoveInputValid(input)) return gameData
+    gameData.copy(board = gameData.placeStone(input, BLACK), turn = gameData.turn + 1)
   }
 
 }
