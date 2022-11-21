58137fbde1b13d5ea4e2d445c1f8669ee2857ba8
da721wol <davwol@gmx.de>
2022-11-21 08:26:14 +0100

    Add missing Tests

--- src/test/scala/de/htwg/msi/controller/ExternalDSLControllerTest.scala
+++ src/test/scala/de/htwg/msi/controller/ExternalDSLControllerTest.scala
@@ -10,5 +10,13 @@ class ExternalDSLControllerTest extends AnyWordSpec {
     "read the content of the provided sgf file and parse it" in {
       externalDSLController.eval("src/test/resources/sgf/2019-04-03-47.sgf") shouldBe Left(externalDSLController.copy(sgfData = SgfData(SgfGameData(19, "MIss", "petgo3"), List(Move("B", "pp"), Move("W", "dp"), Move("B", "dd")))))
     }
+    "try to read the content of the provided sgf file and return an error msg on failure" in {
+      externalDSLController
+        .eval("src/test/resources/sgf/error.sgf")
+        .getOrElse("").replaceAll("[\n|\r]", "").replaceAll(" +", " ") shouldBe """[1.1] failed parsing: string matching regex '[\s\S]*?(?=SZ)' expected but 'Z' found Z[19]^)"""
+    }
+    "return ExternalDSLState as state" in {
+      externalDSLController.getControllerState shouldBe a[ExternalDSLState]
+    }
   }
 }
---
+++ src/test/scala/de/htwg/msi/controller/ExternalDSLStateTest.scala
@@ -0,0 +1,23 @@
+package de.htwg.msi.controller
+
+import de.htwg.msi.model.GameData
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+
+class ExternalDSLStateTest extends AnyWordSpec {
+  "ExternalDSLState" should {
+    val externalDSLState = ExternalDSLState()
+    "return itself on evaluate" in {
+      externalDSLState.evaluate("Any") shouldBe Left(externalDSLState)
+    }
+    "return itself on nextState" in {
+      externalDSLState.nextState(GameData(Nil, 0, 0, Nil)) shouldBe externalDSLState
+    }
+    "return a controller message" in {
+      externalDSLState.getControllerMessage() shouldBe
+        """
+          |Please provide a sgf file for parsing:
+          |""".stripMargin
+    }
+  }
+}
