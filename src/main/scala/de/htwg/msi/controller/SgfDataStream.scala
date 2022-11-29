c57ddb6c782baf70efb530d8a778352857ea9578
Tobias Vogler <67332878+ToVo602@users.noreply.github.com>
2022-11-29 12:44:09 +0100

    first prototyp for akka stream

--- src/main/scala/de/htwg/msi/controller/SgfDataStream.scala
+++ src/main/scala/de/htwg/msi/controller/SgfDataStream.scala
@@ -1,24 +1,34 @@
 package de.htwg.msi.controller
 
 import akka.NotUsed
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, RunnableGraph, Sink, Source}
+import de.htwg.msi.model.SgfData
+import de.htwg.msi.util.Constants
 
 import java.nio.file.{FileSystems, Files, Path}
 import java.util.stream
 import scala.jdk.StreamConverters.*
 
+case class SgfDataStream(externalDSLParser: ExternalDSLParser, dir: String, sink: Sink[SgfData, NotUsed]){
 
-case class SgfDataStream(externalDSLParser: ExternalDSLParser, path: String){
-  val dir: Path = FileSystems.getDefault.getPath(path)
-  val sgfFiles: List[Path] = Files.list(dir).toScala(List)
-                                    .filter(file => Files.isRegularFile(file))
-                                    .filter(file => file.endsWith(".sgf"))
+  def getGraph: RunnableGraph[NotUsed] = {
+    
+    val path: Path = FileSystems.getDefault.getPath(dir)
+    val sgfFiles: List[Path] = Files.list(path).toScala(List)
+      .filter(file => Files.isRegularFile(file))
+      .filter(file => file.endsWith(Constants.sgfFileExtension))
 
-  val source: Source[Path, NotUsed] = Source(sgfFiles)
+    val sgfFileSource: Source[Path, NotUsed] = Source(sgfFiles)
 
-  //TODO: Paths => String in files
+    val sgfFileTextExtraction: Flow[Path, String, NotUsed] = Flow.fromFunction(path => Files.readString(path))
 
-  //TODO: String in files => SgfData
+    val sgfDataExtraction: Flow[String, SgfData, NotUsed] = Flow.fromFunction(fileText => externalDSLParser.parseDSL(fileText))
+      .filter(_.isLeft)
+      .map(_.left.toOption.get)
+
+    sgfFileSource.via(sgfFileTextExtraction).via(sgfDataExtraction).to(sink)
+
+  }
 
 
 }
