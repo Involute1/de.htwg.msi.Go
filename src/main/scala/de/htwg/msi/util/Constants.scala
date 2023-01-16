830b0c7eb995e17d8beb3e018f157a1285071a6b
Tobias Vogler <67332878+ToVo602@users.noreply.github.com>
2023-01-16 14:44:28 +0100

    made path for dir of lots of sgf-files relativ to project

--- src/main/scala/de/htwg/msi/util/Constants.scala
+++ src/main/scala/de/htwg/msi/util/Constants.scala
@@ -3,6 +3,6 @@ package de.htwg.msi.util
 object Constants {
   val alphabetList: List[String] = List("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
   val sgfFileExtensionRegex: String = """.+\.sgf"""
-  val massiveSgfFileLocation = "C:/tobias/HTWG/Vorlesungen/SHJ3/ReactiveSystems/scala/massivedata"
+  val massiveSgfFileLocation = "../massivedata"
 
 }
